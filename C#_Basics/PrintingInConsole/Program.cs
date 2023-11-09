9ba255737fdc4d294b0148eae1a098dd97f1f38e
Aliuser11 <dajmispokojAZ@gmail.com>
2023-11-09 08:01:56 +0100

    Ways to print in console;

---
+++ C#_Basics/PrintingInConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿/* printing datatable in the console */
+static void PrintDataTable(DataTable tbl)
+{
+    string line = "";
+    foreach (DataColumn item in tbl.Columns)
+    {
+        line += item.ColumnName + "   ";
+    }
+    line += "\n";
+    foreach (DataRow row in tbl.Rows)
+    {
+        for (int i = 0; i < tbl.Columns.Count; i++)
+        {
+            line += row[i].ToString() + "   ";
+        }
+        line += "\n";
+    }
+    Console.WriteLine(line);
+}
+PrintDataTable(dataTable2);
+
+
+/* printing lines using JOIN */
+Console.WriteLine(string.Join(", ", objectName()));
+
+
+
+/* printing objects line by line */
+foreach (var item in objectName)
+{
+    global::System.Console.WriteLine(item);
+}

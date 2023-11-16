911ff3838681000fc4ddbd5dfc2b17fd06a21854
Aliuser11 <dajmispokojAZ@gmail.com>
2023-11-16 08:36:36 +0100

    OPP_tutorial part2

---
+++ C#_Basics/OPP_tutorial_Enum/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Enum
+{
+    class Program
+    {
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+        enum Months
+        {
+            January,    // 0
+            February,   // 1
+            March,      // 2
+            April,      // 3
+            May,        // 4
+            June,       // 5
+            July        // 6
+        }
+        enum Months2
+        {
+            January,    // 0
+            February,   // 1
+            March = 6,    // 6 assign your own enum values, and the next items will update their numbers accordingly:
+            April,      // 7
+            May,        // 8
+            June,       // 9
+            July        // 10
+        }
+
+        // throw
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+            }
+            else
+            {
+                Console.WriteLine("Access granted - You are old enough!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Level myVar = Level.Medium;
+            Console.WriteLine(myVar);//Medium
+            // switch statement
+            switch (myVar)
+            {
+                case Level.Low:
+                    Console.WriteLine("Low level");
+                    break;
+                case Level.Medium:
+                    Console.WriteLine("Medium level");
+                    break;
+                case Level.High:
+                    Console.WriteLine("High level");
+                    break;
+            }
+
+
+            int myNum = (int)Months.April; //To get the integer value from an item, you must explicitly convert the item to an int
+            Console.WriteLine(myNum);
+
+            int myNum2= (int)Months2.April;
+            Console.WriteLine(myNum2);
+
+            //throw
+            checkAge(15); // System.ArithmeticException: 'Access denied - You must be at least 18 years old.' 
+            checkAge(20); // Access granted - You are old enough! 
+            // try and catch and finally
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Something went wrong.");
+            }
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished.");
+            }
+        }
+    }
+
+}
---
+++ C#_Basics/OPP_tutorial_Person/Program.cs
@@ -0,0 +1,155 @@
+﻿// Properties and Encapsulation
+class Person
+{
+    //private string name; // field
+
+    //public string Name   // property
+    //{
+    //    get { return name; }   // get method
+    //    set { name = value; }  // set method
+    //}
+    // OR Using automatic properties:
+    public string Name  // property
+    { get; set; }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person myObj = new Person();
+        myObj.Name = "Liam";
+        Console.WriteLine(myObj.Name);
+
+        // Create a myCar object
+        Car myCar = new Car();
+
+        // Call the honk() method (From the Vehicle class) on the myCar object
+        myCar.honk();
+
+        // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+        Console.WriteLine(myCar.brand + " " + myCar.modelName);
+    }
+}
+/*"inheritance concept"  two categories:
+
+    Derived Class (child) - the class that inherits from another class
+    Base Class (parent) - the class being inherited from
+
+To inherit from a class, use the : symbol.*/
+
+class Vehicle  // base class (parent) 
+{
+    public string brand = "Ford";  // Vehicle field
+    public void honk()             // Vehicle method 
+    {
+        Console.WriteLine("Tuut, tuut!");
+    }
+}
+
+class Car : Vehicle  // derived class (child)
+{
+    public string modelName = "Mustang";  // Car field
+}
+
+/*Inheritance lets us inherit fields and methods from another class. Polymorphism uses those methods to perform different tasks*/
+class Animal
+{
+    public virtual void AnimalSound()
+    {
+        Console.WriteLine("The sound");
+    }
+}
+class Pig: Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("pig soound");
+    }
+}
+class Dog:Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("dog soound: bow");
+    }
+}
+
+abstract class Anima //abstract class Animal 
+{
+    public abstract void animalSound();
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+class SmallA : Anima
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The pig says: wee wee");
+    }
+}
+
+//interface
+class Zwierz
+{
+    interface IAnimal
+    {
+        void Sound();
+        void Run();
+    }
+}
+class Kotek : Zwierz
+{
+    public void Sound()
+    {
+        Console.WriteLine("The pig says: wee wee, provided from interface");
+    }
+}
+
+//Multiple Interfaces 
+interface IFirstInterface
+{
+    void myMethod(); // interface method
+}
+
+interface ISecondInterface
+{
+    void myOtherMethod(); // interface method
+}
+class DemoClass : IFirstInterface, ISecondInterface
+{
+    public void myMethod()
+    {
+        Console.WriteLine("Some text from IFirstInterface ..");
+    }
+    public void myOtherMethod()
+    {
+        Console.WriteLine("Some other text from ISecondInterface...");
+    }
+}
+
+
+class Odglosy
+{
+    static void Main()
+    {
+        Animal myAnimal = new();
+        Animal myPig = new Pig();
+        Animal myDog = new Dog();
+        myAnimal.AnimalSound();
+        myPig.AnimalSound();
+        myDog.AnimalSound();
+
+        SmallA mysmall = new ();
+        mysmall.animalSound();
+        mysmall.animalSound();
+        Kotek mykot = new();
+        mykot.Sound();
+
+        DemoClass myObj = new DemoClass();
+        myObj.myMethod();
+        myObj.myOtherMethod();
+
+    }
+}

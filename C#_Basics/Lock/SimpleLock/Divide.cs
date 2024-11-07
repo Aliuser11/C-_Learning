afc78021d7576bd7ae96d0bb2ebf0da99e9844af
Aliuser11 <dajmispokojAZ@gmail.com>
2024-11-07 10:27:48 +0100

    Simple lock;

---
+++ C#_Basics/Lock/SimpleLock/Divide.cs
@@ -0,0 +1,24 @@
+﻿
+internal class DivideMethod : IDivideMethod
+{
+    internal readonly object _locker = new object();
+    static int _val1 = 1, _val2 = 1;
+
+    int IDivideMethod.Divide()
+    {
+        int x = 0;
+        lock (_locker)
+        {
+            if (_val2 != 0)
+            {
+                x = _val1 / _val2;
+                Console.WriteLine(x);
+            }
+        }
+
+        return x;
+    }
+
+}
+
+
---
+++ C#_Basics/Lock/SimpleLock/Program.cs
@@ -0,0 +1,11 @@
+﻿
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        IDivideMethod _method = new DivideMethod();
+        var x = _method.Divide();
+        return x;
+    }
+
+}

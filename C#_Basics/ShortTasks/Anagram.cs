c48dc1216d9ade1d27f45daa8fe7a00e82e2414f
Aliuser11 <dajmispokojAZ@gmail.com>
2026-08-25 09:25:58 +0200

    anagram;

---
+++ C#_Basics/ShortTasks/Anagram.cs
@@ -0,0 +1,24 @@
+﻿namespace ShortPrepareTasks
+{
+    internal class Anagram
+    {
+        //private readonly string s  = "listen";
+        //private readonly string t  = "silent";
+
+        private readonly string s  = "syrop";
+        private readonly string t  = "spory";
+
+        public int CheckAnagram1()
+        {
+            var ss = s.ToCharArray().OrderBy(c => c);
+            var tt = t.ToCharArray().OrderBy(c => c);
+
+            return tt.SequenceEqual(ss) ? 1 : 0;
+        }
+
+        public int CheckAnagram2()
+        {
+            return s.OrderBy(c => c).SequenceEqual(t.OrderBy(c => c)) ? 1 : 0;
+        }
+    }
+}

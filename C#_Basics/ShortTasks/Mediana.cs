f600129a4de726edd3efbbcb4c35940f908c7476
Aliuser11 <dajmispokojAZ@gmail.com>
2026-09-07 21:00:47 +0200

    mediana

---
+++ C#_Basics/ShortTasks/Mediana.cs
@@ -0,0 +1,24 @@
+﻿namespace ShortPrepareTasks
+{
+    internal class Mediana
+    {
+        private readonly int[] arr = [5, 3, 1, 2, 4];
+
+        public int GetMediana()
+        {
+            var lenght = arr.Length;
+            Array.Sort(arr);
+
+            var middle = lenght / 2;
+            if (lenght % 2 == 0)
+            {
+                //licz średnią
+                return Convert.ToInt32( (arr[middle] + arr[middle - 1]) / 2.0);
+            }
+            else
+            {
+                return arr[middle];
+            }
+        }
+    }
+}

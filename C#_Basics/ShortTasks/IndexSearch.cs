d6bfcf5746ca7915490423c6ef0c5d5bd049cc69
Aliuser11 <dajmispokojAZ@gmail.com>
2026-09-15 17:40:39 +0200

    index_search

---
+++ C#_Basics/ShortTasks/IndexSearch.cs
@@ -0,0 +1,32 @@
+﻿namespace ShortPrepareTasks
+{
+    public interface IIndexSearch
+    {
+        int FindTargetIndex1();
+        int FindTargetIndex2();
+        int FirstOccurrence1();
+    }
+    internal class IndexSearch : IIndexSearch
+    {
+        private readonly List<int> nums = [1, 2, 3, 4, 5];
+        private readonly List<int> occur = [1, 2, 2, 3, 4, 5];
+        private readonly int target = 2;
+
+        public int FindTargetIndex1()
+        {
+            var x = nums.FindIndex(x => x == target);
+            return x;
+        }
+
+        public int FindTargetIndex2()
+        {
+            var x = nums.IndexOf(target); // first occurrence
+            return x;
+        }
+
+        public int FirstOccurrence1()
+        {
+            return occur.FindIndex(x => x == target); // first occurrence
+        }
+    }
+}
